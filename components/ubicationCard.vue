0ce82e9d1d34847cb5d3192f8093c5cfbfb947b9
andres857 <andres857@gmail.com>
2024-01-11 16:42:32 -0500

    creacion del layout del dashboard

---
+++ components/ubicationCard.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="max-w-72 overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+        <!-- <img class="object-cover object-center w-full h-56" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt="avatar"> -->
+
+        <img src="https://winbox-assets.sfo3.digitaloceanspaces.com/followtest/IMG_1911.jpg" alt=""  class="object-cover object-center w-full h-44">
+
+        <div class="flex items-center px-6 py-3 bg-red-700">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFfFFF" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z" />
+            </svg>
+
+            <h4 class="mx-3 text-lg font-semibold text-white">Zona QR</h4>
+        </div>
+
+        <div class="px-6 py-4">
+            <h1 class="text-xl font-semibold text-gray-800 dark:text-white">Entrada Hematologia</h1>
+
+            <p class="py-2 text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ducimus, repellat suscipit maxime pariatur</p>
+
+            <div class="flex items-center mt-2 text-gray-700 dark:text-gray-200">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21" />
+                </svg>
+
+                <h1 class="px-2 text-sm">Sede H</h1>
+            </div>
+
+            <div class="flex items-center mt-1 text-gray-700 dark:text-gray-200">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.429 9.75 2.25 12l4.179 2.25m0-4.5 5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0 4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0-5.571 3-5.571-3" />
+                </svg>
+
+                <h1 class="px-2 text-sm"> Piso 1</h1>
+            </div>
+
+            <div class="flex items-center justify-between mt-4 text-gray-700 dark:text-gray-200">
+                <span>Codigo QR</span>
+                <span>Editar</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+  
---
+++ layouts/DashboardLayout.vue
@@ -0,0 +1,51 @@
+<template>
+    <main class=" flex">
+        <section>
+            <sideBar/>
+        </section>
+
+        <section class=" flex w-full min-h-screen gap-6 justify-center items-center ">
+            <!-- content section -->
+            <div class="h-full w-full grid ">
+                <div class=" h-[8vh] flex justify-around items-center">
+                    <h3> Zonas QR </h3>
+                    <h3> Crear </h3>
+                </div>
+                <!-- search and filter section -->
+                <div class="h-[12vh] flex justify-center items-center ">
+                    <!-- <input type="text"> -->
+                    
+                    <searchUbication class="w-6/12"/>
+                </div>
+                <!-- content cards section -->
+                <div class="h-[80vh] grid grid-cols-5 gap-x-4 gap-y-5 bg-slate-200 p-5 overflow-y-auto">
+                    <ubicationCard class="h-[490px]"/>
+                    <ubicationCard class="h-[490px]"/>
+                    <ubicationCard class="h-[490px]"/>
+                    <ubicationCard class="h-[490px]"/>
+                    <ubicationCard class="h-[490px]"/>
+                    <ubicationCard class="h-[490px]"/>
+                    <ubicationCard class="h-[490px]"/>
+                    <ubicationCard class="h-[490px]"/>
+                    <ubicationCard class="h-[490px]"/>
+                    <ubicationCard class="h-[490px]"/>
+                    <ubicationCard class="h-[490px]"/>
+                    <ubicationCard class="h-[490px]"/>
+                    <ubicationCard class="h-[490px]"/>
+                    <ubicationCard class="h-[490px]"/>
+                    <ubicationCard class="h-[490px]"/>
+                </div>
+            </div>
+            <!-- <ubicationCard2 class="h-[320px]"/> -->
+        </section>
+
+    </main>
+
+</template>
+
+<script lang="ts" setup>
+    import sideBar from '~/components/sideBar.vue';
+    import searchUbication from '~/components/searchUbication.vue';
+    import ubicationCard from '~/components/ubicationCard.vue';
+    import ubicationCard2 from '~/components/ubicationCardv2.vue';
+</script>
