e4ebb662ef7095a91207100a48bcb907a22e47e8
andres857 <andres857@gmail.com>
2024-02-01 08:01:09 -0500

    cambios intermedios

---
+++ components/ContentUbications.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="h-[80vh] grid grid-cols-3 gap-x-4 gap-y-5 bg-slate-200 p-5 overflow-y-auto">
+        <UbicationCard class="h-[255px]" v-if="sidebarStore.currentSelection === 'Inicio'" 
+            v-for=" ubication in ubicationStore.ubications" :key="ubication.id" :nameUbication="ubication.name" :imageUrl="ubication.imageUrl" :description="'Lorem ipsum dolor sit amet consectetur adipisicing elit...'" :location="ubication.location" :floor="ubication.floor" :type="ubication.ubicationType"/>
+        
+        <UbicationCard  class="h-[255px]" v-else-if="sidebarStore.currentSelection === 'Destino'" v-for=" ubication, index in ubicationStore.ubications" :key="index" :nameUbication="ubication.name" :imageUrl="ubication.imageUrl" :description="'Lorem ipsum dolor sit amet consectetur adipisicing elit...'" :location="ubication.location" :floor="ubication.floor" :type="ubication.ubicationType"/>
+
+        <UbicationCard  class="h-[255px]" v-else-if="sidebarStore.currentSelection === 'Etapa'" v-for=" ubication, index in ubicationStore.ubications" :nameUbication="ubication.name" :imageUrl="ubication.imageUrl" :description="'Lorem ipsum dolor sit amet consectetur adipisicing elit...'" :location="ubication.location" :floor="ubication.floor" :type="ubication.ubicationType"/>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import UbicationCard from '~/components/UbicationCard.vue';
+    const ubicationStore = useUbicationsStore();
+    const sidebarStore = useSidebarStore();
+</script>
--- layouts/DashboardLayout.vue
+++ layouts/DashboardLayout.vue
@@ -33,18 +33,9 @@
                     <searchUbication class="w-[650px]"/>
                 </div>
 
-                <!-- content cards section -->
-                <div class="h-[80vh] grid grid-cols-3 gap-x-4 gap-y-5 bg-slate-200 p-5 overflow-y-auto">
-                    <ubicationCard class="h-[255px]" v-if="sidebarStore.currentSelection === 'Inicio'" 
-                        v-for=" ubication in ubicationStore.ubications" :key="ubication.id" :nameUbication="ubication.name" :imageUrl="ubication.imageUrl" :description="'Lorem ipsum dolor sit amet consectetur adipisicing elit...'" :location="ubication.location" :floor="ubication.floor" :type="ubication.ubicationType"/>
-                    
-                    <ubicationCard  class="h-[255px]" v-else-if="sidebarStore.currentSelection === 'Destino'" v-for=" ubication, index in ubicationStore.ubications" :key="index" :nameUbication="ubication.name" :imageUrl="ubication.imageUrl" :description="'Lorem ipsum dolor sit amet consectetur adipisicing elit...'" :location="ubication.location" :floor="ubication.floor" :type="ubication.ubicationType"/>
+                <!-- DashBoard Content -->
+                <!-- renderizar el content de manera condicional -->
 
-                    <ubicationCard  class="h-[255px]" v-else-if="sidebarStore.currentSelection === 'Etapa'" v-for=" ubication, index in ubicationStore.ubications" :nameUbication="ubication.name" :imageUrl="ubication.imageUrl" :description="'Lorem ipsum dolor sit amet consectetur adipisicing elit...'" :location="ubication.location" :floor="ubication.floor" :type="ubication.ubicationType"/>
-
-                    <createNewUbication class="col-span-3 justify-center w-full" v-else-if="sidebarStore.currentSelection === 'CreateNewUbication'"/>
-                    <resourceCreatedModal v-else-if="modalStore.modal"/>
-                </div>
             </div>
         </section>
     </main>
@@ -54,8 +45,6 @@
     import { computed } from 'vue';
     import sideBar from '~/components/sideBar.vue';
     import searchUbication from '~/components/searchUbication.vue';
-    import ubicationCard from '~/components/ubicationCard.vue';
-    import createNewUbication from '~/components/forms/createNewUbication.vue';
     import resourceCreatedModal from '~/components/modals/resourceCreatedModal.vue';
 
     import { useSidebarStore } from '~/stores/sidebar';
@@ -67,7 +56,6 @@
     const modalStore = useModalStatusStore();
 
     const showCreateNewUbication = ()=>{
-        // modalStore.showModal();
         sidebarStore.currentSelection = 'CreateNewUbication';
     }
 
